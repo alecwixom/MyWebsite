fa4cdf0fe57fbc012fce29387b81c7c1844266ed
Alec Wixom <atwixom@gmail.com>
2023-08-03 16:14:10 -0600

    More styling and added more to the projects page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,20 @@
 import './home.css'
+import picture from '../images/portrait.jpg'
 
 const home = () => {
     return (
         <div className="intro">
             <div className='background'>
             </div>
-            <h1>Hello, I'm Alec Wixom!</h1>
-            <h2>Full-Stack developer who enjoy's the art of coding, games and camping. I am located in Utah County, Utah.</h2>
+            <div>
+                <h1>Hello, I'm Alec Wixom!</h1>
+            </div>
+            <div className='portrait'>
+                <img className='homeimg' src={picture} alt="portrait" />
+            </div>
+            <p className='intro-text'>
+                "Full-Stack developer who enjoy's the art of coding, games and camping. I am located in Utah County, Utah."
+            </p>
         </div>
     )
 }
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,10 +1,33 @@
 import './projects.css'
+import liftshare from '../images/LiftShare.png'
+import chat from '../images/Chat.png'
+
 
 const projects = () => {
-    return(
+    return (
         <div className="projects">
-            <h1>Projects</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ipsa, soluta magni, fugit ab, beatae praesentium eos quidem molestiae aliquam explicabo quisquam. Hic, tempora possimus nulla odit ratione officiis dolorum.</p>
+            <h1 className='protitle'>Projects</h1>
+            <p>
+                [ - Description of page - ]
+            </p>
+            <div className='liftsharebox'>
+                <a className='liftlink' target='_blank' href="https://github.com/alecwixom/CapstoneSpecs">
+                    <img className='liftshareimg' src={liftshare} alt="LiftShare" />
+                </a>
+                <p className='liftdesc'>
+                    LiftShare is my Capstone project while at DevMountain, this project contains axios requests, postgresSQL and other functionalities that were a blast to work on.
+                    Click the image to see the github repo!
+                </p>
+            </div>
+            <div className='chatbox'>
+                <a className='chatlink' href="">
+                    <img className='chatimg' src={chat} alt="chatpng" />
+                </a>
+                <p className='liftdesc'>
+                    AnonymoChat is my Capstone Website I created during foundations at DevMountain. This project uses axios requests, built in database and the use of socket.io for
+                    fast communication from client to server in messaging.
+                    </p>
+            </div>
         </div>
     )
 }
